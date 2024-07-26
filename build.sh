3bc79bb2d22624f14a7f6c2b651ea82384f40895
Aleksandr Savgirya <poriesto@hotmail.com>
2024-07-26 22:18:31 +0300

    Add information about build to README. Correct bug with astra container archive

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://1drv.ms/u/s!Aic8RIOhSb1wo6drmG62SULxVeQSDQ?e=A7wXvW
+#wget https://1drv.ms/u/s!Aic8RIOhSb1wo6drmG62SULxVeQSDQ?e=FAb8GM
 docker load -i astra-python.tar
 docker build -f web-app/Dockerfile -t sms:175 .
 docker build -f sms-db/Dockerfile -t sms-db:175 .
